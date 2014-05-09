67fecc5cedd1087ce4d3c8a8aaa3a4ca457d9ed5
Ken Hu <whosbacon@gmail.com>
2014-05-09 17:08:52 +0800

    populate insert features

--- db.py
+++ db.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import os
+import json
 
 import config
 
-def save(result):
-    pass
+def get_host():
+    return os.environ.get('CLOUDANT_URL') or config.load()['COUCH']
+
+def save(result, id_=None):
+    if id_: result['_id'] = id_
+    if 'datetime' in result: result['datetime'] = str(result['datetime'])
+    return requests.post(get_host() + '/', data=json.dumps(result))
   
 def load_summary():
     pass
