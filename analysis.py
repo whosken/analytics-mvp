35c68b057d0dc98822432a761fae8d114c314d53
Ken Hu <whosbacon@gmail.com>
2014-05-11 23:36:48 +0800

    bucket sentiment analysis for better reporting

--- analysis.py
+++ analysis.py
@@ -11,13 +11,16 @@ to_blob = textblob.Blobber()
 
 def work():
     tweets,finished = queue.pop(100)
+    if not tweets: return
     db.save(*map(analyze, tweets))
     finished()
+    return len(tweets)
 
 def analyze(tweet):
     created_at = datetime.datetime.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+    sentiment = to_blob(tweet['text']).sentiment.polarity
     return {'text':tweet['text'],
-            'sentiment':to_blob(tweet['text']).sentiment,
+            'sentiment':get_sentiment(tweet['text']),
             '_id':unicode(tweet['id']),
             'datetime':[created_at.year,
                         created_at.month,
@@ -27,7 +30,17 @@ def analyze(tweet):
                         created_at.second
                         ]
             }
+  
+def get_sentiment(text):
+    sentiment = to_blob(text).sentiment.polarity
+    if sentiment > 0: return 'pos'
+    if sentiment < 0: return 'neg'
+    return 'neu'
 
+    
 if __name__ == '__main__':
+    import time    
     while True:
-        work()
+        if not work():
+            print 'sleeping...'
+            time.sleep(2)
