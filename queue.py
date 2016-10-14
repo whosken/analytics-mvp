2f090beaa60450019ddf7cb7fcdb101d53ded2d9
Ken Hu <whosbacon@gmail.com>
2016-10-14 13:19:28 +0100

    dont use static queue

--- queue.py
+++ queue.py
@@ -6,15 +6,19 @@ import json
 
 import config
 
-client = iron_mq.IronMQ(
-    project_id=config.get('IRONIO.PROJECT','IRON_MQ_PROJECT_ID'),
-    token=config.get('IRONIO.TOKEN','IRON_MQ_TOKEN'))
-queue = client.queue('analyze_this')
+def get_client():
+    return iron_mq.IronMQ(
+        project_id=config.get('IRONIO.PROJECT','IRON_MQ_PROJECT_ID'),
+        token=config.get('IRONIO.TOKEN','IRON_MQ_TOKEN'))
+        
+def get_queue():
+    return client.queue('analyze_this')
 
 def push(*messages):
-    return queue.post(*map(json.dumps, messages))
+    return get_queue().post(*map(json.dumps, messages))
 
 def pop(max=10):
+    queue = get_queue()
     messages = queue.get(max)['messages']
     message_ids = [m['id'] for m in messages]
     delete_messages = get_delete_messages(queue, message_ids)
